4596e247dec632c848a5abfe423467a255d26af4
Wenderson Farias <wendersonpfarias@gmail.com>
2025-05-11 01:16:47 -0400

    adiciona script para calcular a soma dos números de 1 a 13

---
+++ Questão_1/index.js
@@ -0,0 +1,14 @@
+var INDICE = 13, SOMA = 0, K = 0;
+
+while (K < INDICE) {
+  K = K + 1;
+  SOMA = SOMA + K;
+
+}
+
+console.log(`Resultado da soma: ${SOMA}`);
+
+
+//Ao final do processamento, qual será o valor da variável SOMA ?
+
+// Resposta: 91
